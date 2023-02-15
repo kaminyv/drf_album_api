4691770c1573d5fc5266fa534dc72f8f483e2d76
kaminyv <kaminyv@yandex.ru>
2023-02-15 14:56:20 +0600

    Added view for the api application.

--- api/views.py
+++ api/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework.views import Response, APIView
+from .models import Artist
+from .serializers import ArtistSerializer
 
-# Create your views here.
+
+class ArtistAPIView(APIView):
+    def get(self, *args, **kwargs):
+        queryset = Artist.objects.all()
+        serializer = ArtistSerializer(queryset, many=True)
+        return Response(serializer.data)
