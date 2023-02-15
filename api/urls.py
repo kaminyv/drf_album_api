27ce591b573c630309d82df3d4128507a0075487
kaminyv <kaminyv@yandex.ru>
2023-02-15 16:09:04 +0600

    Added swagger for the api application.

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,26 @@
-from django.urls import path
-from .views import ArtistAPIView
+from django.urls import path, re_path
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from .views import ArtistList, ArtistDetail
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title='API for api application',
+        default_version='v1',
+        description='Documentation for the api test application',
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
 
 urlpatterns = [
-    path('', ArtistAPIView.as_view(), name='artist-api-view')
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$',
+            schema_view.without_ui(cache_timeout=0),
+            name='schema-json'),
+    re_path(r'^swagger/$',
+            schema_view.with_ui('swagger', cache_timeout=0),
+            name='schema-swagger-ui'),
+    path('', ArtistList.as_view(), name='artist-list'),
+    path('<int:pk>', ArtistDetail.as_view(), name='artist-details')
 ]
--- api/views.py
+++ api/views.py
@@ -1,11 +1,30 @@
 from django.shortcuts import render
 from rest_framework.views import Response, APIView
+from rest_framework.generics import ListAPIView
+from rest_framework import status
+from drf_yasg.utils import swagger_auto_schema
 from .models import Artist
 from .serializers import ArtistSerializer
 
 
-class ArtistAPIView(APIView):
+class ArtistList(ListAPIView):
+    """
+    Returns a list of artist objects and their albums and songs.
+    """
+    queryset = Artist.objects.all()
+    serializer_class = ArtistSerializer
+
+
+class ArtistDetail(APIView):
+    @swagger_auto_schema(responses={200: ArtistSerializer()})
     def get(self, *args, **kwargs):
-        queryset = Artist.objects.all()
-        serializer = ArtistSerializer(queryset, many=True)
-        return Response(serializer.data)
+        """
+        Returns detailed information about the artist.
+        """
+        try:
+            artist = Artist.objects.get(pk=kwargs.get('pk'))
+        except Artist.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        serializer = ArtistSerializer(artist)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
