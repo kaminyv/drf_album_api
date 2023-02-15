6e40f6d2e0b63e2b249abea3484990ef337751bf
kaminyv <kaminyv@yandex.ru>
2023-02-15 14:48:56 +0600

    Added models in the admin panel.

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Artist, Song, Album, Track
 
-# Register your models here.
+
+@admin.register(Artist)
+class ArtistAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Song)
+class Song(admin.ModelAdmin):
+    pass
+
+
+class TrackAdminInline(admin.TabularInline):
+    model = Track
+    extra = 1
+
+
+@admin.register(Album)
+class AlbumAdmin(admin.ModelAdmin):
+    inlines = (TrackAdminInline,)
